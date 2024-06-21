533a6b1e256b10dbb27ab9cf5e159a3ffbb77802
blessing-user <blessing.o.oboh@gmail.com>
2024-06-21 11:59:49 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.53.95:9000/',
+  serverUrl: 'http://18.233.66.134:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
